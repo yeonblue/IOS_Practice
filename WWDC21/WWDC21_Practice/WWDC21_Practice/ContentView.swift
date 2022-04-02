bf69ab3e32debee0dd14405cde3caee9299a29e1
최서연 <yeondublue@naver.com>
2022-04-02 12:44:47 +0900

    first commit

---
+++ WWDC21/WWDC21_Practice/WWDC21_Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WWDC21_Practice
+//
+//  Created by yeonBlue on 2022/04/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WWDC21/WWDC21_Practice/WWDC21_Practice/WWDC21_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC21_PracticeApp.swift
+//  WWDC21_Practice
+//
+//  Created by yeonBlue on 2022/04/02.
+//
+
+import SwiftUI
+
+@main
+struct WWDC21_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
